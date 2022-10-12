7488598e622b2879de3eddab9e4705379671d800
TruongNBN <mylove.lanhuong@gmail.com>
2022-10-12 20:13:06 +0700

    init app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Tạo component App
+function App() {
+    return (
+        <div>
+            sSami App!
+        </div>
+    )
+}
+
+// Render component App vào #root element
+ReactDOM.render(<App />, document.getElementById('root'))
+
